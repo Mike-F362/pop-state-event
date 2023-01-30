2cbc572cce613447d0c6c5dd042fe67c1cb52536
Mike-F362 <55290897+Mike-F362@users.noreply.github.com>
2023-01-30 10:09:51 +0100

    set header & default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {Page1Component} from './page1/page1.component';
-import {Page2Component} from './page2/page2.component';
-
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
-    {path: 'page-1', component: Page1Component},
-    {path: 'page-2', component: Page2Component},
+  { path: 'page-1', component: Page1Component },
+  { path: 'page-2', component: Page2Component },
+  { path: '', redirectTo: '/page-1', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
